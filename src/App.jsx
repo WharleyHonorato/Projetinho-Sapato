0cf073ea9d0a8e03b26f424933a5193ba184638f
WharleyHonorato <wharleygrant@gmail.com>
2024-07-12 15:43:40 -0300

    mud

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import { IoSearchSharp } from "react-icons/io5";
 import { PiShoppingCartSimpleLight } from "react-icons/pi";
 import { RiUser3Fill } from "react-icons/ri";
 import { IoIosHeartEmpty } from "react-icons/io";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [produtos, setProdutos] = useState([])
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => setProdutos(data))
+  },[])
+
   return (
-    <div className="w-full h-full bg-black">
-      <div className="w-full h-[120px] bg-white flex">
+    <div className="w-full h-full bg-white">
+      <div className="w-full h-[130px] bg-white flex">
         <div className="pl-10 gap-5 flex items-center">
-          <img className="w-[180px]" src="https://images.vexels.com/media/users/3/291084/isolated/preview/06b9fe2146fcd1517e80e939f43c2fda-roupa-de-sapateado-de-dana-a-simples.png" alt="" />
-          <p className="font-bold italic text-xl"><strong>Wharley's Shoes: aqui você encontra o seu par perfeito!</strong></p>
+          <img className="w-[200px]" src="https://img.freepik.com/vetores-premium/modelo-de-logotipo-de-vetor-de-icone-de-loja-de-loja_917138-2077.jpg" alt="" />
+          <p className="italic text-lg"><strong>Wharley's Clothing</strong></p>
         </div>
         <div className="w-full">
-          <div className=" w-[90%] flex justify-center items-center h-full">
+          <div className=" w-[110%] flex justify-center items-center h-full">
             <div className="flex items-center justify-center bg-black p-2 rounded-2xl h-[32%]">
               <input className=" outline-none pl-5 w-full h-full bg-black text-white"
                 type="text" placeholder="O que você está procurando?"
@@ -31,18 +40,17 @@ function App() {
           <PiShoppingCartSimpleLight size={35} /><p>Carrinho</p>
         </div>
       </div>
-      <div className="w-full h-[50vh] flex pl-20 mt-10 gap-10">
-        <div className="w-[150px] h-[150px] bg-sky-600 rounded-2xl">
-
-        </div>
-        <div className="w-[150px] h-[150px] bg-red-800 rounded-2xl">
-          
-        </div>
-        <div className="w-[150px] h-[150px] bg-fuchsia-300 rounded-2xl">
-          
-        </div>
-        <div className="w-[150px] h-[150px] bg-lime-200 rounded-2xl">
-          
+      <div className="mt-5">
+        <div className="flex flex-wrap w-full h-screen justify-center gap-5">
+          {produtos.map((produto) => 
+            <div className="text-black w-[400px] h-[400px] bg-white p-2 border-4 rounded-2xl border-black overflow-hidden flex items-center justify-center flex-col"
+            key={produto.id}>
+              <p className="">{produto.title}</p>
+              <img className="h-[80px]" src={produto.image} alt={produto.title} />
+              <p>{produto.price}</p>
+              <button className="">Comprar</button>
+            </div>
+          )}
         </div>
       </div>
     </div>
